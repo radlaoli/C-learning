0f15ff3b45cdf811c2b35160bd177fab8e579725
Radla Oliveira <radlaoliveira5@gmail.com>
2025-09-27 11:54:24 -0300

    att1

--- Bee/bee1198.c
+++ Bee/bee1198.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
-int main(){
-    int i = 2, j=0;
-    for(j = 0; j<3;j++){
-        printf("I=%d J=%d\n", i,j+i+1);
-    } 
+
+int main() {
+    int a, b;
+    double i, j;
+    for (a = 0; a <= 10; a++) {
+        i = a * 0.2;
+        for (b = 1; b <= 3; b++) {
+            j = b + i;
+            if (a % 5 == 0) {
+                printf("I=%d J=%d\n", (int)i, (int)j);
+            } else {
+                printf("I=%.1f J=%.1f\n", i, j);
+            }
+        }
+    }
     return 0;
 }
